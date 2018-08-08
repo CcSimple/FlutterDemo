eb2901bf06daceb93611fc80d15117eedaed8ee1
CcSimple <840054486@qq.com>
2018-08-08 16:49:21 +0800

    dio网络请求

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:FlutterDemo/pages/home.dart';
 import 'package:FlutterDemo/pages/frame/frame1.dart';
 import 'package:FlutterDemo/pages/frame/frame2.dart';
 import 'package:FlutterDemo/pages/frame/frame3.dart';
+import 'package:FlutterDemo/pages/network//net.dart';
 import 'package:FlutterDemo/pages/frame/pages/index.dart';
 import 'package:FlutterDemo/pages/frame/pages/order.dart';
 import 'package:FlutterDemo/pages/frame/pages/mine.dart';
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
         '/frame1': (BuildContext context) => new Frame1Page(),
         '/frame2': (BuildContext context) => new Frame2Page(),
         '/frame3': (BuildContext context) => new Frame3Page(),
+        '/net': (BuildContext context) => new NetPage(),
         '/index': (BuildContext context) => new IndexPage(),
         '/order': (BuildContext context) => new OrderPage(),
         '/mine': (BuildContext context) => new MinePage(),
